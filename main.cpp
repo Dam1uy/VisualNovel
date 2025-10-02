69cd63557267fe54277dd95e83451679ea5f12c1
Den1s <kyotore121@gmail.com>
2025-10-02 21:49:11 +0300

    a

---
+++ main.cpp
@@ -0,0 +1,9 @@
+﻿#include "Engine.hpp"
+#include <iostream>
+
+int main() {
+    Engine engine;
+    engine.Init("Player1");  // numele jucătorului
+    engine.Loop();           // pornește bucla jocului
+    return 0;
+}
